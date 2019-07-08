9fb7de25101460828004ffbf3814567da3fb65db
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-07-08 23:48:09 +0200

    Add modal, loop, object

--- js/script.js
+++ js/script.js
@@ -8,11 +8,19 @@ var output = document.getElementById('output');
 var result = document.getElementById('result');
 var btnNewGame = document.getElementById('newgame');
 var roundsNumber = document.getElementById('round-number');
+var text = document.getElementById('alert');
+
+//zmienne w obiekcie
+var params = {
+  playerScore: 0,
+  compScore: 0,
+  roundsToWin: 0
+}
 
 //zmienne
-var playerScore = 0;
+/*var playerScore = 0;
 var compScore = 0;
-var roundsToWin = 0;
+var roundsToWin = 0;*/
 
 //nieaktywne buttony
 function buttonDisabled() {
@@ -27,6 +35,35 @@ function buttonEnabled() {
   btnRock.disabled = false;
   btnScissors.disabled = false;
 }
+
+//funkcja otwierająca modal
+function showModal(){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.add('show');
+  var modals = document.querySelectorAll('.modal');
+  for (var i = 0; i < modals.length; i++) {
+    modals[i].classList.remove('show');
+    modals[i].classList.add('show');
+  }
+  if (params.PlayerScore >= params.roundsToWin){
+    text.innerHTML = 'YOU WON !!!';
+  }
+  else if (params.compScore >= params.roundsToWin){
+    text.innerHTML = 'YOU LOST !!!';
+  }
+}
+
+//funkcja zamykająca modal po kliknięciu na elemencie z klasą close
+var hideModal = function (event) {
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+}
+
+var closeButtons = document.querySelectorAll('.modal .close');
+for (var i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener('click', hideModal);
+}
+
 
 
 //funkcja generująca ruch komputera na podstawie wylosowanej liczby
@@ -48,10 +85,10 @@ var compare = function(userMove, compChoice){
 } else if ((userMove == 'paper') && (compChoice == 'rock') ||
     (userMove == 'rock') && (compChoice == 'scissors') ||
     (userMove == 'scissors') && (compChoice == 'paper')) {
-      playerScore++;
+      params.PlayerScore++;
       return 'YOU WON';
 } else {
-    compScore++;
+    params.compScore++;
     return 'YOU LOST';
     }
 };
@@ -68,10 +105,10 @@ btnScissors.addEventListener('click', function(){
 });*/
 
 //pętla przchodząca przez wszytskie buttony
-var btnAll = document.querySelectorAll('player-move');
+var btnAll = document.querySelectorAll('.player-move');
 
 for(var i = 0; i < btnAll.length; i++){
-  var dataMove = btnAll[i].getAtribute('data-move');
+  var dataMove = btnAll[i].getAttribute('data-move');
   btnAll[i].addEventListener('click', function(){
     playerMove(dataMove);
   })
@@ -84,39 +121,41 @@ function outputResult(compareResult, playerText, compText){
 
 //funkcja wyświetlająca wynik rozgrywki (sumuje poszczególne rundy)
 var scoresResult = function(){
-  result.innerHTML = 'PLAYER ' + playerScore + ' COMPUTER ' + compScore;
+  result.innerHTML = 'PLAYER ' + params.PlayerScore + ' COMPUTER ' + params.compScore;
 }
 
 //nowa gra 
 btnNewGame.addEventListener('click', function(){
   resetGame()
-  roundsToWin = window.prompt('How many round would you like to play?')
-  if(roundsToWin.length < 1 || NaN) {
+  params.roundsToWin = window.prompt('How many round would you like to play?')
+  if(params.roundsToWin.length < 1 || NaN) {
     roundsNumber.innerHTML = 'Write a number';
   }
   else {
-    roundsNumber.innerHTML = 'Play till ' + roundsToWin + ' won rounds';
+    roundsNumber.innerHTML = 'Play till ' + params.roundsToWin + ' won rounds';
     buttonEnabled();
   }
 });
 
 //funkcja resetująca grę
 var resetGame = function(){
-  playerScore = 0;
-  compScore = 0;
+  params.PlayerScore = 0;
+  params.compScore = 0;
   result.innerHTML = ''
   output.innerHTML = ''
 }
 
 //koniec gry
 function endOfGame() {
-  if (playerScore >= roundsToWin){
+  if (params.PlayerScore >= params.roundsToWin){
     output.innerHTML = 'YOU WON !!! <br> click new game to start';
     buttonDisabled();
+    showModal();
   }
-  else if (compScore >= roundsToWin){
+  else if (params.compScore >= params.roundsToWin){
     output.innerHTML = 'YOU LOST !!! <br> click new game to start';
     buttonDisabled();
+    showModal();
   }
 }
 
